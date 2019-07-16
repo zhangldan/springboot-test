837db639e47514d192958b985d4ee1e473746d7c
grampus-ci@missfresh.cn <>
2019-07-16 08:40:04 +0800

    add00

--- Test4/src/main/java/org/sang/Main.java
+++ Test4/src/main/java/org/sang/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         UseFunctionService bean = context.getBean(UseFunctionService.class);
-        System.out.println(bean.sayHello("sang"));
+        System.out.println(bean.sayHello("sang02"));
         context.close();
     }
 }
